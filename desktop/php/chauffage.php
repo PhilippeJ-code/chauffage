9dc79e29ae19a4aa3a25676a6977519c7d22b708
PhilippeJ-code <jacobs.philippe@skynet.be>
2024-04-21 10:03:48 +0200

    Jeedom v4.4

--- desktop/php/chauffage.php
+++ desktop/php/chauffage.php
@@ -269,6 +269,18 @@ foreach (jeedom::getConfiguration('eqLogic:category') as $key => $value) {
       <div role="tabpanel" class="tab-pane" id="commandtab">
         <a class="btn btn-success btn-sm cmdAction pull-right" data-action="add" style="margin-top:5px;"><i class="fa fa-plus-circle"></i> {{Commandes}}</a><br/><br/>
         <table id="table_cmd" class="table table-bordered table-condensed">
+          <thead>
+            <tr>
+              <th>{{Id}}</th>
+              <th>{{Nom}}</th>
+              <th>{{Type}}</th>
+              <th>{{Paramètres}}</th>
+              <th>{{Etat}}</th>
+              <th>{{Action}}</th>
+            </tr>
+          </thead>
+          <tbody>
+          </tbody>
         </table>
       </div>
     </div>
